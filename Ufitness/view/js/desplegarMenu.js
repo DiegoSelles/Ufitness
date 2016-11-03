16529828dabc9aadd2946962488fee55198e66e4
Srconde <srconde@esei.uvigo.es>
2016-11-03 09:38:29 +0100

    Desplegables solo se cierran al clicar en el boton

--- Ufitness/view/js/desplegarMenu.js
+++ Ufitness/view/js/desplegarMenu.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// desplegar menu
-	document.querySelector("#nivel1").onclick = function() {
+	document.querySelector("#activador_1").onclick = function() {
 		var menuItems = document.querySelectorAll("#nivel1 ul");
 		for (i = 0; i<menuItems.length; i++) {
 			menuItems[i].classList.toggle("visible");
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 
-	document.querySelector("#nivel2").onclick = function() {
+	document.querySelector("#activador_2").onclick = function() {
 		var menuItems = document.querySelectorAll("#nivel2 ul");
 		for (i = 0; i<menuItems.length; i++) {
 			menuItems[i].classList.toggle("visible");
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 
-	document.querySelector("#nivel3").onclick = function() {
+	document.querySelector("#activador_3").onclick = function() {
 		var menuItems = document.querySelectorAll("#nivel3 ul");
 		for (i = 0; i<menuItems.length; i++) {
 			menuItems[i].classList.toggle("visible");
