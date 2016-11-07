c88dae780f8e0b4d7bf8b077b332ecc8cc95e4c7
DiegoSelles <diegoselles10@gmail.com>
2016-11-08 00:53:29 +0100

    Delete conexion.php

--- Ufitness/view/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$conexion = mysql_connect('localhost','root','root') or die("Error al conectar". mysql_error());
-	$basededatos= "Ufitness";
-	$db = mysql_select_db($basededatos, $conexion) or die("Error al seleccionar la bd". mysql_error());
-
-?>
