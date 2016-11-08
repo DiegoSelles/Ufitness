65d6e97416884b9fedbc7adfb8923c91ea29652b
ddduran95 <danyel_ou@hotmail.es>
2016-11-08 17:16:58 +0100

    cambios nombre BD

--- Ufitness/resources/conexion.php
+++ Ufitness/resources/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	$conexion = mysql_connect('localhost','root','root') or die("Error al conectar". mysql_error());
-	$basededatos= "Ufitness";
+	$basededatos= "G24";
 	$db = mysql_select_db($basededatos, $conexion) or die("Error al seleccionar la bd". mysql_error());
 
 ?>
