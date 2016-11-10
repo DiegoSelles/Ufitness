44d52e36a917059eef55009f85a588a1ecbe71e0
Srconde <srconde@esei.uvigo.es>
2016-11-10 20:23:51 +0100

    Add files via upload
    Se ven los datos de la base de datos y se pueden eliminar actividades

--- Ufitness/controller/ActividadController.php
+++ Ufitness/controller/ActividadController.php
@@ -1,9 +1,29 @@
 <?php
-
+require_once("../resources/conexion.php");
 class ActividadController{
-	
+
 	function reservarPlaza(){
 	   header("Location: indexAdmin.php");
+	}
+
+	function listarActividades (){
+		$consulta = mysql_query("SELECT * FROM Actividad");
+		$listaActividades = array();
+		while ($actividad = mysql_fetch_assoc($consulta)) {
+				array_push($actividades, $actividad);
+		}
+		$hola = "hola";
+		return $hola;
+	}
+
+	function getActividad ($idActividad){
+			$consulta = mysql_query("SELECT * FROM Actividad WHERE idActividad = $idActividad");
+			$actividad = mysql_fetch_assoc($consulta);
+			return $actividad;
+	}
+	function eliminarActividad ($idActividad){
+		mysql_query("DELETE FROM Actividad WHERE idActividad = $idActividad");
+
 	}
 }
 
