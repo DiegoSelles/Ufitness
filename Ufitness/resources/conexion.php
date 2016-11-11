1e6ff68132111a426dfdc4fb843903288a30f75e
DiegoSelles <diegoselles10@gmail.com>
2016-11-11 19:47:15 +0100

    nuevo conexion

--- Ufitness/resources/conexion.php
+++ Ufitness/resources/conexion.php
@@ -1,6 +1,11 @@
 <?php
-	$conexion = mysql_connect('localhost','root','root') or die("Error al conectar". mysql_error());
+/*	$conexion = mysql_connect('localhost','root','root') or die("Error al conectar". mysql_error());
 	$basededatos= "G24";
 	$db = mysql_select_db($basededatos, $conexion) or die("Error al seleccionar la bd". mysql_error());
+*/
+$connect = new mysqli('localhost','root','root','G24');
+if(mysqli_connect_errno()){
+		unset($connect);
+	}
 
 ?>
