00428ff8001f022c462d11a3c29cfae379785e5a
ddduran95 <danyel_ou@hotmail.es>
2016-11-16 16:19:32 +0100

    Update EntrenamientoMapper.php

--- Ufitness/model/EntrenamientoMapper.php
+++ Ufitness/model/EntrenamientoMapper.php
@@ -5,34 +5,21 @@ require_once("../model/Deportista.php");
 
 
 class EntrenamientoMapper {
-	
-  public function findAllEntrenamientos() {
-    $stmt = $this->db->query("SELECT * FROM Entrenamiento");
-    $entrenamientos_db = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $entrenamientos = array();
 
-    foreach ($entrenamientos_db as $entrenamiento) {
-      $deportista = new Deportista($entrenamiento["Deportista_DNI"]);
-      array_push($entrenamientos, new Entrenamiento($entrenamiento["idEntrenamiento"], $deportista, $entrenamiento["duracion"]));
+  public function buscarEntrenamientoId($id){
+
+    global $connect;
+  	$consulta ="SELECT * FROM Entrenamiento WHERE idEntrenamiento='".$id."' ";
+  	$resultado = $connect->query($consulta);
+    $entrenamiento = mysqli_fetch_assoc($resultado);
+
+  	if($entrenamiento!=NULL) {
+  			return  new Entrenamiento($entrenamiento["duracion"],$entrenamiento["nombre"],$entrenamiento["Deportista_DNI"], $entrenamiento["idEntrenamiento"] );
+  	}else {
+      return NULL;
     }
 
-    return $entrenamientos;
-  }
-
-  public function findEntrenamientoById($idEntrenamiento){
-    $stmt = $this->db->prepare("SELECT * FROM Entrenamiento WHERE id=?");
-    $stmt->execute(array($idEntrenamiento));
-    $entrenamiento = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if($entrenamiento != null) {
-      return new Entrenamiento(
-      $entrenamiento["idEntrenamiento"],
-      new Deportista($entrenamiento["Deportista_DNI"]),
-      $entrenamiento["duracion"]);
-      } else {
-        return NULL;
-      }
     }
 
 
@@ -41,8 +28,8 @@ class EntrenamientoMapper {
     $stmt->execute(array($entrenamiento->getDeportista()->getDni(),$entrenamiento->getDuracion()));
     return $this->db->lastInsertId();
   }
-  
-  	function listarEntrenamientos (){
+
+  public function listarEntrenamientos (){
 		global $connect;
 		$consulta ="SELECT * FROM Entrenamiento ";
 		$resultado = $connect->query($consulta);
@@ -55,7 +42,32 @@ class EntrenamientoMapper {
 		return $listaEntrenamientos;
 
 	}
-	
+
+	public function listarEntrenamientosNivel ($nivel){
+  	global $connect;
+  	$consulta ="SELECT * FROM Entrenamiento WHERE nivel='".$nivel."' ";
+  	$resultado = $connect->query($consulta);
+  	$listaEntrenamientos = array();
+  	while ($actual = mysqli_fetch_assoc($resultado)) {
+  			$entrenamiento = new Entrenamiento($actual["duracion"],$actual["nombre"],$actual["Deportista_DNI"], $actual["idEntrenamiento"] );
+  			array_push($listaEntrenamientos, $entrenamiento);
+  	}
+  	return $listaEntrenamientos;
+
+	}
+
+  public function ejerciciosEntrenamiento($id){
+    global $connect;
+  	$consulta ="SELECT * FROM Entrenamiento_has_Ejercicio WHERE Entrenamiento_idEntrenamiento='".$id."' ";
+  	$resultado = $connect->query($consulta);
+  	$listaEjercicios = array();
+    while ($actual = mysqli_fetch_assoc($resultado)) { /*#############*/
+        $consulta ="SELECT * FROM Ejercicio WHERE idEjerc='".$id."' ";
+  			array_push($listaEjercicios, $ejercicio);
+  	}
+  	return $listaEntrenamientos;
+  }
+
   public function update(Entrenamiento $entrenamiento) {
     $stmt = $this->db->prepare("UPDATE Entrenamiento set duracion=? where id=?");
     $stmt->execute(array($entrenamiento->getDuracion(), $entrenamiento->getidEntrenamiento()));
