69a27109017bdd15d5f0a0dc057ae1da35dee522
ddduran95 <danyel_ou@hotmail.es>
2016-11-16 17:11:03 +0100

    Add files via upload

---
+++ Ufitness/model/EntrenamientoHasEjercicio.php
@@ -0,0 +1,46 @@
+<?php
+
+class EntrenamientoHasEjercicio
+{
+	public $idEntrenamiento;
+	public $idEjercicio;
+	public $seriesxRep;
+	public $carga;
+
+
+	function __construct($idEntrenamiento,$idEjercicio,$seriesxRep=NULL,$carga=NULL)
+  {
+		$this ->idEntrenamiento = $idEntrenamiento;
+		$this ->idEjercicio = $idEjercicio;
+		$this ->seriesxRep = $seriesxRep;
+		$this ->carga = $carga;
+
+	}
+
+  public function getIdEntrenamiento() {
+    return $this ->idEntrenamiento;
+  }
+
+  public function getIdEjercicio() {
+    return $this ->idEjercicio;
+  }
+
+  public function getSxP() {
+    return $this ->seriesxRep;
+  }
+
+  public function setSxR ($seriesxRep){
+    $this ->seriesxRep = $seriesxRep;
+  }
+
+  public function getCarga() {
+    return $this ->carga;
+  }
+
+  public function setCarga ($carga){
+    $this ->carga = $carga;
+  }
+
+}
+
+?>
