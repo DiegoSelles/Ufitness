452908554cb810eed42e5c17e2ebdcf2141280c6
Em2406 <ebrodriguez2@esei.uvigo.es>
2016-11-18 18:18:10 +0100

    Update controlador_Entrenamiento.php
    ejercicioDiario

--- Ufitness/controller/controlador_Entrenamiento.php
+++ Ufitness/controller/controlador_Entrenamiento.php
@@ -220,6 +220,13 @@ class controlador_Entrenamiento{
      header("Location: ../view/monitorizarEntrenamiento.php?idEntrenamiento=$idEntrenamiento&idEjercicio=$idEjercicio");
  
    }
+	public function ejercicioDiario($dniDeportista,$idEntrenamiento,$idEjercicio,$fecha)
+  {
+    $entrenamientoMapper = new EntrenamientoMapper();
+  
+  return  $entrenamientoMapper->ejercicioDiario($dniDeportista,$idEntrenamiento,$idEjercicio,$fecha);
+
+  }
 
     public function asignarEntrenamiento($dni,$nombre){
 	  global $connect;
