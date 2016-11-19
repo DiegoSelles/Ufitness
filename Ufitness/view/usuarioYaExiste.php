839be78c5062b43a8b41963af4b33726f379886d
Em2406 <ebrodriguez2@esei.uvigo.es>
2016-11-19 01:40:32 +0100

    Add files via upload

---
+++ Ufitness/view/usuarioYaExiste.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Añadir Entrenador - Ufitness</title>
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<h1> ERROR </h1>  
+<br/>
+<h2> USUARIO YA EXISTE <h2>
+<br/>
+<a id="submit" href="crearEntrenador.php" class="btn btn-primary" type="button">Volver</a>
+</body>
