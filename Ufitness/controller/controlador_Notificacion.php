35c96f693310036a84b5291e05a701ccca00f038
ddduran95 <danyel_ou@hotmail.es>
2017-01-04 22:29:12 +0100

    Add files via upload

--- Ufitness/controller/controlador_Notificacion.php
+++ Ufitness/controller/controlador_Notificacion.php
@@ -1,26 +1,61 @@
 <?php
 require_once("../resources/conexion.php");
 require_once("../model/NotificacionMapper.php");
+require_once("../model/DeportistaMapper.php");
 require_once("../model/Notificacion.php");
+require_once("../model/Deportista.php");
 
 
 class controlador_Notificacion{
 
 	private $notificacionMapper;
+	private $deportistaMapper;
+
+	public function __construct() {
+
+    $this->notificacionMapper = new NotificacionMapper();
+		$this->deportistaMapper = new DeportistaMapper();
+  }
 
 	public function newNotificacion(){
 		$notificacionMapper = new NotificacionMapper();
-		global $connect;
+		$deportistaMapper = new DeportistaMapper();
 
 		$titulo = $_POST['titulo'];
     $descripcion = $_POST['descripcion'];
-		$receptor = $_POST['receptor'];
-		$notificacion= new Notificacion ($titulo,$descripcion,$_SESSION["Dni"],$receptor);
-		$notificacionMapper->save($notificacion);
+		$receptor=$_POST['receptor'];
+
+		if($receptor=="todos"){
+			$deportistas = $deportistaMapper->listarDeportistas();
+			foreach ($deportistas as $deportista) {
+				$notificacion = new Notificacion($titulo,$descripcion, $_SESSION["Dni"],$deportista->getDni());
+				$notificacionMapper->save($notificacion);
+			}
+		}else{
+
+			$receptores =$_POST['receptores'];
+
+			if(empty($receptores))
+			{
+				echo "No has añadido usuarios";
+			}
+			else
+			{
+				$N = count($receptores);
+				for($i=0; $i < $N; $i++)
+				{
+					$notificacion = new Notificacion($titulo,$descripcion, $_SESSION["Dni"],$receptores[$i]);
+					$notificacionMapper->save($notificacion);
+				}
+			}
+		}
+		header ("Location: ../view/adminIndex.php");
 
 	}
 
-	
+	public function viewNotificacion($receptor){
+		return $this->notificacionMapper->listarNotificacionesReceptor($receptor);
+	}
 
 	public function delete(){
 		if (!isset($_POST["idActividad"])) {
@@ -35,9 +70,7 @@ class controlador_Notificacion{
 
 	}
 	public function notificacionVista(){
-
-
-
+		print("hadfasjdf");
 	}
 
 
