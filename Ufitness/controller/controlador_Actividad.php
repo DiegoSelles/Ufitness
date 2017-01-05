79824a0a8b08514038264eee4477d75bf3c2b48c
DiegoSelles <diegoselles10@gmail.com>
2017-01-05 11:06:02 +0100

    Mejora reservarPlaza

--- Ufitness/controller/controlador_Actividad.php
+++ Ufitness/controller/controlador_Actividad.php
@@ -94,26 +94,7 @@ class controlador_Actividad{
 	}
 
 	public function reservarPlaza($idActividad,$usuarioActual){
-	   global $connect;
-	   $consulta = "SELECT numPlazas FROM Actividad WHERE idActividad ='" .$idActividad. "'" ;
-	   $resultado= $connect->query($consulta);
-	   $plazas = mysqli_fetch_assoc($resultado);
-	   $query = "SELECT count(idReserva) FROM Reserva WHERE Deportista_Usuario_Dni = '".$usuarioActual."' and Actividad_idActividad ='".$idActividad."'";
-	   $result = $connect->query($query);
-	   $existe = mysqli_fetch_assoc($result);
-
-		if($plazas['numPlazas'] == 0){
-				echo '<script language="javascript">alert("No quedan plazas disponibles para esta actividad");</script>';
-		}else if($existe['count(idReserva)'] != 0 ){
-			echo '<script language="javascript">alert("Ya tienes una plaza reservada en esta actividad");</script>';
-			}else{
-				$plazasRestantes = $plazas['numPlazas'] - 1;
-				$plazasOcupadas = 0;
-				$plazasOcupadas++;
-				mysqli_query($connect,"UPDATE Actividad SET numPlazas = '" .$plazasRestantes. "' WHERE idActividad ='" .$idActividad. "'");
-				mysqli_query($connect,"INSERT INTO Reserva(Deportista_Usuario_Dni,Actividad_idActividad,fecha,plazas_ocupadas) VALUES('" .$_SESSION['Dni']."', '" .$idActividad."', '" .date("Y-m-d")."', '" .$plazasOcupadas."')");
-				echo '<script language="javascript">alert("Has reservado plaza en esta actividad");window.location.href="../view/adminActividades.php";</script>';
-			}
+		return $this->actividadMapper->reservar($idActividad,$usuarioActual);
 
 		}
 
