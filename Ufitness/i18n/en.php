f061d5d15b9a1f70cba17e47b03978154520847e
DiegoSelles <diegoselles10@gmail.com>
2017-01-09 12:24:01 +0100

    Carpeta con idiomas

---
+++ Ufitness/i18n/en.php
@@ -0,0 +1,107 @@
+<?php
+$texts = array(
+	//Dropdown
+	'Entrar' => 'Sign in',
+	'DNI del usuario' => 'User DNI',
+	'Contraseña' => 'Password',
+	'Recordarme' => 'Remember me',
+	'Perfil' => 'Profile',
+	'Opciones' => 'Options',
+	'Desconectarse' => 'Sign out',
+	
+	//index
+	'Consulta nuestras actividades' => 'Check out our activities',
+	'Accede a la web con contenidos deportivos de todo tipo' => 'Access to WebSite with sports content of all type',
+	'Visita nuestras instalaciones' => 'Visit our installations',
+	'¡Ven a conocernos!'=> 'Come to meet us',
+	'¿A qué esperas?' => 'what are you waiting for?',
+	'Vente' => 'Come',
+	
+	
+	//Wrapper
+	'Actividades' => 'Activities',
+	'Entrenamientos' => 'Trainings',
+	'Ejercicios' => 'Exercises',
+	'Entrenadores' => 'Trainers',
+	'Deportistas' => 'Athletics',
+	
+	//adminIndex
+	'Bienvenido a Ufitness' => 'Welcome to Ufitness',
+	'Nueva notificacion' => 'New notice',
+	'Ultimas notifaciones' => 'latest news',
+	'No tienes notificaciones nuevas.' => 'You haven´t new notifications.',
+	'Visto' => 'Viewed',
+	'¡Tienes nuevas notificaciones!' => 'You have new notifications!',
+	
+	//adminActividades && userActividades
+	'Lista de Actividades' => 'Activities list',
+	'Buscar Actividad' => 'Search activity',
+	'Añadir Actividad' => 'Add activity',
+	'Horario' => 'Horary',
+	'Tipo de actividad' => 'Type of activity',
+	'Numero de plazas' => 'Capacity',
+	'Lugar' => 'Place',
+	'Editar' => 'Edit',
+	'Eliminar' => 'Remove',
+	
+	//Crear notificaciones
+	'Título' => 'Title',
+	'Descripción' => 'Description',
+	'Enviar a' => 'Send to',
+	'Todos' => 'All',
+	'Elegir deportistas:' => 'Choose Athletics:',
+	'Enviar Notificación' => 'Send notification',
+	
+	//adminEntrenadores
+	'Lista de Entrenadores' => 'Trainers list',
+	'Añadir Entrenador' => 'Add trainer',
+	'Edad' => 'Age',
+	'modificar' => 'Edit',
+	
+	//Crear entrenador
+	'Nuevo Entrenador' => 'New trainer',
+	'Nombre completo' => 'Full name',
+	'El formato debe coincidir con 8 números y 1 letra.' => 'Format must coincide with 8 numbers and 1 word.',
+	'Fecha Nacimiento' => 'Date of birth',
+	'Añadir' => 'Add',
+	
+	//Eliminar entrenador
+	'¿Está seguro que quiere eliminar a este usuario?' => 'Are you sure that do you want remove this user?',
+	'Nombre' => 'Name',
+	'SI' => 'YES',
+	'NO' => 'NO',
+	
+	//Modificar entrenador'Riesgos'
+	'Modificar Entrenador' => 'Edit trainer',
+	'Guardar Cambios' => 'Save changes',
+	'Volver' => 'Back',
+	
+	//adminDeportistas
+	'Lista de Deportistas' => 'Trainers Athletics',
+	'Buscar Deportista' => 'Search Athletics',
+	'Registrar Deportista' => 'Add Athletics',
+	'Riesgos' => 'Risks',
+	'Asignar Entrenamiento' => 'Assign training',
+	
+	//crear deportista
+	'Nuevo Deportista' => 'New Athletic',
+	'Tipo Deportista' => 'Athletic type',
+	'Registrar' => 'Register',
+	
+	//eliminar deportista
+	'¿Está seguro que quiere eliminar a este deportista?' => 'Are you sure that do you want remove this athletic?',
+	
+	//Modificar deportista
+	'Modificar Deportista' => 'Edit Athletic',
+	'Tipo Actual' => 'Actual Type',
+	'Modificar Tipo Deportista' => 'Edit Athletic type',
+	
+	//Asignar entrenamiento
+	'Asignacion de entrenamientos' => 'Training assignment',
+	'Asignar' => 'Assign',
+	'Crear entrenamiento' => 'Create trainer'
+	
+	
+	
+);
+?>
---
+++ Ufitness/i18n/es.php
@@ -0,0 +1,104 @@
+<?php
+$texts = array(
+	//Dropdown
+	'Sign in' => 'Entrar',
+	'User DNI' => 'DNI del usuario',
+	'Password' => 'Contraseña',
+	'Remember me' => 'Recordarme',
+	'Profile' => 'Perfil',
+	'Options' => 'Opciones',
+	'Sign out' => 'Desconectarse',
+	
+	//index
+	'Check out our activities' => 'Consulta nuestras actividades',
+	'Access to WebSite with sports content of all type' => 'Accede a la web con contenidos deportivos de todo tipo',
+	'Visit our installations' => 'Visita nuestras instalaciones',
+	'Come to meet us' => '¡Ven a conocernos!',
+	'what are you waiting for?' => '¿A qué esperas?',
+	'Come' => 'Vente',
+	
+	//Wrapper 
+	'Activities' => 'Actividades',
+	'Trainings' => 'Entrenamientos',
+	'Exercises' => 'Ejercicios',
+	'Trainers' => 'Entrenadores',
+	'Athletics' => 'Deportistas',
+	
+	//adminIndex
+	'Welcome to Ufitness' => 'Bienvenido a Ufitness',
+	'New notice' => 'Nueva notificacion',
+	'latest news' => 'Ultimas notifaciones',
+	'You haven´t new notifications.' => 'No tienes notificaciones nuevas.',
+	'Viewed' => 'Visto',
+	'You have new notifications!' => '¡Tienes nuevas notificaciones!',
+	
+	//adminActividades && userActividades
+	'Activities list' => 'Lista de Actividades',
+	'Search activity' => 'Buscar Actividad',
+	'Add activity' => 'Añadir Actividad',
+	'Horary' => 'Horario',
+	'Type of activity' => 'Tipo de actividad',
+	'Capacity' => 'Numero de plazas',
+	'Place' => 'Lugar',
+	'Edit' => 'Editar',
+	'Remove' => 'Eliminar',
+	
+	//Crear notificaciones
+	'Title' => 'Título',
+	'Description' => 'Descripción',
+	'Send to' => 'Enviar a',
+	'All' => 'Todos',
+	'Choose Athletics:' => 'Elegir deportistas:',
+	'Send notification' => 'Enviar Notificación',
+	
+	//adminEntrenadores
+	'Trainers list' => 'Lista de Entrenadores',
+	'Add trainer' => 'Añadir Entrenador',
+	'Age' => 'Edad',
+	'Edit' => 'modificar',
+	
+	//Crear entrenador
+	'New trainer' => 'Nuevo Entrenador',
+	'Full name' => 'Nombre completo',
+	'Format must coincide with 8 numbers and 1 word.' => 'El formato debe coincidir con 8 números y 1 letra.',
+	'Date of birth' => 'Fecha Nacimiento',
+	'Add' => 'Añadir',
+	
+	//Eliminar entrenador
+	'Are you sure that do you want remove this user?' => '¿Está seguro que quiere eliminar a este usuario?',
+	'Name' => 'Nombre',
+	'YES' => 'SI',
+	'NO' => 'NO',
+	
+	//Modificar entrenador
+	'Edit trainer' => 'Modificar Entrenador',
+	'Save changes' => 'Guardar Cambios',
+	'Back' => 'Volver',
+	
+	//adminDeportistas
+	'Trainers Athletics' => 'Lista de Deportistas',
+	'Search Athletics' => 'Buscar Deportista',
+	'Add Athletics' => 'Registrar Deportista',
+	'Risks' => 'Riesgos',
+	'Assign training' => 'Asignar Entrenamiento',
+	
+	//crear deportista
+	'New Athletic' => 'Nuevo Deportista',
+	'Athletic type' => 'Tipo Deportista',
+	'Register' => 'Registrar',
+	
+	//Eliminar deportista
+	'Are you sure that do you want remove this athletic?' => '¿Está seguro que quiere eliminar a este deportista?',
+	
+	//Modificar deportista
+	'Edit Athletic' => 'Modificar Deportista',
+	'Actual Type' => 'Tipo Actual',
+	'Edit Athletic type' => 'Modificar Tipo Deportista',
+	
+	//Asignar Entrenamiento
+	'Training assignment' => 'Asignacion de entrenamientos',
+	'Asignar' => 'Assign',
+	'Crear entrenamiento' => 'Create trainer'
+);
+?>
+
